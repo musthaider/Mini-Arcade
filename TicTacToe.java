892d44de4bec9798a8aca555a096ca48300907ac
Ossien <Oasiedu@umw.mail.edu>
2025-04-22 13:54:12 -0400

    new code

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,16 +1,28 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
 
 public class TicTacToe {
 
 
     private char[][] board = new char[3][3];
-    private String currentPlayer = "X";
+    private char currentPlayer = 'X';
     private GameMode mode;
 
     public TicTacToe(GameMode mode) {
+            this.mode = mode;
+            resetGame();
+    }
 
+    public void resetGame() {
+        for (int index = 0; index < 3; index++ )
+            for(int indx = 0; indx <3; indx++ )
+                    board[index][indx] = ' ';
+        currentPlayer ='X';
+    }
+    public boolean makeMove(int row, int col) {
+        if (board[row][col]==' '){
+            board[row][col] = currentPlayer;
+            return true;
+        }
+        return false;
     }
 
     public char[][] getBoard() {
