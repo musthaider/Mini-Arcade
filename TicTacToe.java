ffe4260a827c63f8b029d80f614652629388ae51
Ossien <Oasiedu@umw.mail.edu>
2025-04-23 18:56:07 -0400

    new code

--- TicTacToe.java
+++ TicTacToe.java
@@ -24,41 +24,82 @@ public class TicTacToe {
         }
         return false;
     }
+    public char checkWinner() {
+        // Rows and Columns
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] != ' ' &&
+                    board[i][0] == board[i][1] &&
+                    board[i][1] == board[i][2]) {
+                return board[i][0];
+            }
+            if (board[0][i] != ' ' &&
+                    board[0][i] == board[1][i] &&
+                    board[1][i] == board[2][i]) {
+                return board[0][i];
+            }
+        }
 
-    public void makeComputerMove() {
-        for (int aiChoice = 0; aiChoice < 3; aiChoice++) {
-            for (int aichoose = 0; aichoose < 3; aichoose++) {
-                if (board[aiChoice][aichoose] == ' '){
-                    board[aiChoice][aichoose] = currentPlayer;
+        // Diagonals
+        if (board[0][0] != ' ' &&
+                board[0][0] == board[1][1] &&
+                board[1][1] == board[2][2]) {
+            return board[0][0];
+        }
+
+        if (board[0][2] != ' ' &&
+                board[0][2] == board[1][1] &&
+                board[1][1] == board[2][0]) {
+            return board[0][2];
+        }
+
+        return ' '; // No winner
+    }
+
+
+    public void computerMove() {
+        if (tryWinningMove('O')) return;
+        if (tryWinningMove('X')) return;
+
+        if (board[1][1] == ' ') {
+            board[1][1] = 'O';
+            return;
+        }
+
+        int[][] corners = {{0,0},{0,2},{2,0},{2,2}};
+        for (int[] corner : corners) {
+            if (board[corner[0]][corner[1]] == ' ') {
+                board[corner[0]][corner[1]] = 'O';
+                return;
+            }
+        }
+
+        for (int r = 0; r < 3; r++) {
+            for (int c = 0; c < 3; c++) {
+                if (board[r][c] == ' ') {
+                    board[r][c] = 'O';
                     return;
                 }
-
             }
-
         }
     }
-    public char checkWinner() {
-        for (int win = 0; win < 3; win++) {
-            if (board[win][0] != ' ' && board[win][0] ==
-                board [win][1] && board[win][1] == board[win][2])
-                return board[win][0];
 
-            if (board[0][win] != ' ' && board [0][win] == board [1][win]&&
-                    board[1][win] == board[2][win])
-                return board[0][win];
+    private boolean tryWinningMove(char player) {
+        for (int r = 0; r < 3; r++) {
+            for (int c = 0; c < 3; c++) {
+                if (board[r][c] == ' ') {
+                    board[r][c] = player;
+                    if (checkWinner() == player) {
+                        if (player == 'O') return true;
+                        board[r][c] = 'O'; // block
+                        return true;
+                    }
+                    board[r][c] = ' ';
+                }
+            }
         }
-
-        if (board[0][0]!= ' ' && board[0][0] ==
-        board[1][1] && board[1][1] == board[2][2])
-            return board [0][0];
-
-        if (board[0][2] != ' ' && board[0][2] == board[1][1] &&
-                board[1][1] == board[2][0])
-            return board[0][2];
-
-
-        return ' ';
+        return false;
     }
+
     public boolean isBoardFull() {
         for(int index = 0; index<3; index++)
             for(int indx = 0; indx <3; indx++)
